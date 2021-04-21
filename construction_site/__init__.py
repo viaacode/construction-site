5f79519467ab9a3ddb490e4b60f99f41d3cf4d81
Miel Vander Sande <miel.vandersande@meemoo.be>
2021-04-21 15:38:42 +0200

    DEV-1614 Initializes repo for RDF construction library

---
+++ construction_site/__init__.py
@@ -0,0 +1,11 @@
+"""
+construction-site.
+
+A usable and scalable API for RDF construction.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Miel Vander Sande'
+__credits__ = 'meemoo vzw'
+
+from .parse_functions import parse_json
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='construction_site',
+    version='0.0.1',    
+    description='A usable and scalable API for RDF construction.',
+    url='https://github.com/viaacode/constructionsite',
+    author='Miel Vander Sande',
+    author_email='miel.vandersande@meemoo.be',
+    license='BSD 2-clause',
+    packages=['construction_site'],
+    install_requires=[
+        'rdflib>=5.0.0'                    
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
