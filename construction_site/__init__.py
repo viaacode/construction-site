69393d7f2c8e536132ef9b60a861b17aeaab4cc4
Miel Vander Sande <miel.vandersande@meemoo.be>
2021-06-18 17:13:42 +0200

    Added airflow hook for sparql update.

--- construction_site/__init__.py
+++ construction_site/__init__.py
@@ -9,4 +9,5 @@ __author__ = 'Miel Vander Sande'
 __credits__ = 'meemoo vzw'
 
 from .parse_functions import parse_json, parse_dict
+from .airflow.hooks.sparql_update import SparqlUpdateHook
 #from .construction_site import ConstructionSite
---
+++ tests/sparql_update_test.py
@@ -0,0 +1,6 @@
+import pytest
+
+from construction_site.airflow.hooks.sparql_update import SparqlUpdateHook
+
+def test_construction():
+    SparqlUpdateHook(method='GET')
